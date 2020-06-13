9ca316e7da5acf76d60ca010cbf472cc66420750
jayy-tee <49388196+jayy-tee@users.noreply.github.com>
2020-06-13 16:16:54 +1000

    feat: Add Domain Unit Tests

---
+++ src/Identity.Domain.UnitTests/TestData/ValidLegacyUser.cs
@@ -0,0 +1,21 @@
+using Identity.Common;
+using Identity.Domain.UserAggregate;
+
+namespace Identity.Domain.UnitTests.TestData
+{
+    public static class ValidLegacyUser
+    {
+        public static string Firstname = "theFirstName";
+        public static string Lastname = "theLastName";
+        public static string Username = "theUser";
+        public static string EmailAddress = "theUser@somedomain.com";
+        public static string Password = "thePassword";
+        public static UserStatus Status = UserStatus.Enabled;
+        public static UserSource UserSource = UserSource.Legacy;
+
+        public static User AsUser() => new User(
+                   Firstname, Lastname, Username,
+                   EmailAddress, Status, Password,
+                   UserSource);
+    }
+}
