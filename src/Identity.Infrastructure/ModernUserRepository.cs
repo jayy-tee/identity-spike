8261a66565b818c42e962447abe8744f42ea3d3b
John Taylor <john.taylor@servcorp.com.au>
2020-06-14 12:57:59 +1000

    feat: Convert ModernUserRepository to MSSQL

--- src/Identity.Infrastructure/ModernUserRepository.cs
+++ src/Identity.Infrastructure/ModernUserRepository.cs
@@ -1,36 +1,75 @@
 using System;
+using System.Data;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
 using Identity.Application.Users;
 using Identity.Common;
 using Identity.Domain.UserAggregate;
 
-
 namespace Identity.Infrastructure
 {
-    public class ModernUserRepository : IUserRepository
+    public class ModernUserRepository : IUserRepository, IDisposable
     {
-        private List<User> _users;
-        public UserSource SourceSystem { get; private set; }
+        private readonly IDbConnection _db;
+        private const string _connString = "Server=win10vm.local;Database=Identity_Spike;User Id=identity_spike;Password=IdentitySpikepa44word;";
+        public void Dispose()
+        {
+            _db.Close();
+        }
 
+        public UserSource SourceSystem { get; private set; }
         public ModernUserRepository()
         {
-            _users = new List<User>() {
-                 new User("Joe", "Bloggs", "jbloggs1", "fake@fake.com", UserStatus.Disabled, "blah", UserSource.New),
-                 new User("Joe", "Bloggs", "jbloggs", "fake@fake.com", UserStatus.Enabled, "blah", UserSource.New)
-            };
+            _db = new SqlConnection(_connString);
             SourceSystem = UserSource.New;
         }
 
-        public async Task<User> Get(string username) =>
-             _users.Where(u => u.Username == username).FirstOrDefault();
-             
-        public async Task<bool> CheckExists(string username) =>
-            _users.Where(u => u.Username == username).Count() > 0;
-        
-        public async Task<bool> AddUser(User user) =>
-            throw new NotImplementedException();
+        public async Task<User> Get(string username)
+        {
+            return await _db.QueryFirstOrDefaultAsync<User>(
+                @"
+                SELECT firstname, lastname, username, email as 'emailAddress', status, 2 AS 'source', passwordHash AS '_passwordHash',
+                passwordSalt AS '_passwordSalt', status as userStatus 
+                FROM dbo.Users WHERE username=@username"
+                , new { @username = username }
+            );
+        }
+        public async Task<bool> CheckExists(string username)
+        {
+            return await _db.QueryFirstAsync<int>("SELECT COUNT(*) FROM dbo.Users WHERE username=@username", new { @username = username }) > 0;
+        }
+
+        public async Task<bool> AddUser(User user)
+        {
+            if (_db.State != ConnectionState.Open)
+                _db.Open();
+                
+            var tran = _db.BeginTransaction();
+            try {
+                await _db.ExecuteAsync(
+                    @"INSERT INTO dbo.Users (firstname, lastname, username, email, status, passwordHash, passwordSalt)
+                    VALUES(@firstname, @lastname, @username, @email, @status, @passwordHash, @passwordSalt)",
+                    new {
+                        @username = user.Username,
+                        @firstname = user.FirstName,
+                        @lastname = user.LastName,
+                        @email = user.EmailAddress,
+                        @passwordHash = user.GetPasswordHash(),
+                        @passwordSalt = user.GetPasswordSalt(),
+                        @status = user.UserStatus
+                    },
+                    tran);
+                tran.Commit();
+                return true;
+            } 
+            catch (Exception ex)
+            {
+                tran.Rollback();
+                throw ex;
+            }
+        }
 
     }
 }
