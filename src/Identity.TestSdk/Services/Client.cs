83bf614b4ec07ce08f7feb8d9ff962c80dae70ea
jayy-tee <49388196+jayy-tee@users.noreply.github.com>
2020-06-15 00:29:45 +1000

    WIP: Acceptance Test Imrpovements

--- src/Identity.TestSdk/Services/Client.cs
+++ src/Identity.TestSdk/Services/Client.cs
@@ -17,7 +17,33 @@ namespace Identity.TestSdk.Services
             _logger = logger;
         }
 
+        public IResponse Execute(IRequest request)
+        {
+            var response = ExecuteRequestViaRestSharp(request);
+
+            _logger.Write(response.IsSuccessful ? 
+                Serilog.Events.LogEventLevel.Information : 
+                Serilog.Events.LogEventLevel.Error,
+                "{Event} {HttpMethod} {BaseUrl}{RelativeUrl} {StatusCode}", "TestEndHttpRequest", request.Method, _testSettings.BaseUrl, request.RelativeUrl, response.StatusCode);
+
+            return new Response(response);
+        }
+
         public IResponse Execute(IRequest request, HttpStatusCode andExpect)
+        {
+            var response = ExecuteRequestViaRestSharp(request);
+
+            _logger.Write(response.StatusCode == andExpect ? 
+                Serilog.Events.LogEventLevel.Information : 
+                Serilog.Events.LogEventLevel.Error,
+                "{Event} {HttpMethod} {BaseUrl}{RelativeUrl} {StatusCode} {ExpectedStatusCode}", "TestEndHttpRequest", request.Method, _testSettings.BaseUrl, request.RelativeUrl, response.StatusCode, andExpect);
+
+            if (response.StatusCode != andExpect) throw new WebException($"The call to {request.Method} {_testSettings.BaseUrl}{request.RelativeUrl} was expected to return {andExpect} but returned {response.StatusCode}");
+
+            return new Response(response);
+        }
+
+        private RestSharp.IRestResponse ExecuteRequestViaRestSharp(IRequest request)
         {
             var rc = new RestSharp.RestClient(_testSettings.BaseUrl);
             var rr = new RestSharp.RestRequest(request.RelativeUrl, request.Method);
@@ -28,16 +54,7 @@ namespace Identity.TestSdk.Services
             }
 
             _logger.Information("{Event} {HttpMethod} {BaseUrl}{RelativeUrl} {@request}", "TestStartHttpRequest", request.Method, _testSettings.BaseUrl, request.RelativeUrl, request);
-            var response = rc.Execute(rr);
-
-            _logger.Write(response.StatusCode == andExpect ? 
-                Serilog.Events.LogEventLevel.Information : 
-                Serilog.Events.LogEventLevel.Error,
-                "{Event} {HttpMethod} {BaseUrl}{RelativeUrl} {StatusCode} {ExpectedStatusCode}", "TestEndHttpRequest", request.Method, _testSettings.BaseUrl, request.RelativeUrl, response.StatusCode, andExpect);
-
-            if (response.StatusCode != andExpect) throw new WebException($"The call to {request.Method} {_testSettings.BaseUrl}{request.RelativeUrl} was expected to return {andExpect} but returned {response.StatusCode}");
-
-            return new Response(response);
+            return rc.Execute(rr);
         }
     }
 }
